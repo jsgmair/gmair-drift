499103f17bfa6da6aa63c4f74e62e638c018a88c
lawrenceZL <40664153+lawrenceZL@users.noreply.github.com>
2019-07-25 14:49:58 +0800

    get openid

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 //app.js
 App({
+  globalData: {
+    openid: '',
+    protocol: 'https://',
+    url: 'microservice.gmair.net',
+    port: 443,
+  },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    this.check_session(this)
+  },
+  check_session: function (context) {
+    let that = this;
+    wx.checkSession({
+      success: function () {
+        let openid = wx.getStorageSync('openid');
+        // console.log(openid)
+        if (openid == undefined || openid == '') {
+          // console.log('login')
+          that.login();
+        }
+      },
+      fail: function () {
+        // console.log('fail')
+        that.login();
+      },
+      complete: function () {
+        // console.log('complete')
+        let openid = wx.getStorageSync('openid');
+        that.globalData.openid = openid;
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
+  },
+  login: function () {
+    let that = this;
+    // console.log('login')
+    wx.login({
+      success(response) {
+        let code = response.code;
+        console.log(code)
+        that.code2openid(code);
+      }
+    });
+  },
+  code2openid: function (code) {
+    let that = this;
+    wx.request({
+      url: 'https://microservice.gmair.net/drift/user/openid',
+      method: "POST",
+      data: {
+        code: code
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+      },
+      success: function (response) {
+        console.log(response);
+        response = response.data;
+        if (response.responseCode == 'RESPONSE_OK') {
+          let openid = response.data;
+          wx.setStorage({
+            key: 'openid',
+            data: openid,
           })
         }
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
 })
