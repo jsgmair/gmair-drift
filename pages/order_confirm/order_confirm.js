7fa8de5a99c03f80585170dc12c519d36a6e861e
lawrenceZL <40664153+lawrenceZL@users.noreply.github.com>
2019-07-29 15:37:08 +0800

    order confirm

--- pages/order_confirm/order_confirm.js
+++ pages/order_confirm/order_confirm.js
@@ -1,19 +1,84 @@
 // pages/order_confirm/order_confirm.js
+const util = require('../../utils/util.js')
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    order_id:''
+    order_id:"",
+    address:'',
+    address_detail:'',
+    name:'',
+    phone:'',
+    equip_name:'',
+    item_quantity:'',
+    realPay:''
   },
-
+  obtain_order_detail(order_id){
+    let that = this
+    console.log(order_id)
+    wx.request({
+      url: app.globalData.protocol + app.globalData.url + '/drift/order/'+order_id,
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+      },
+      success: function (response) {
+        console.log(response)
+        response = response.data
+        if (response.responseCode === "RESPONSE_OK") {
+          that.setData({
+            address: response.data.province + response.data.city + response.data.district,
+            address_detail: response.data.address,
+            name: response.data.consignee,
+            phone: response.data.phone,
+            equip_name: response.data.list[0].itemName,
+            item_quantity: response.data.list[0].quantity,
+            realPay:response.data.realPay*100
+          })
+        }
+      }
+    })
+  },
+  onClickButton(e){
+    let that = this
+    console.log('支付')
+    wx.request({
+      url: app.globalData.protocol + app.globalData.url + '/drift/order/payment/'+that.data.order_id+'/info',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+      },
+      success: function (response) {
+        console.log(response)
+        response = response.data
+        if (response.responseCode === "RESPONSE_OK") {
+            wx.requestPayment({
+              timeStamp: response.data[0].tradeStartTime+'',
+              nonceStr: response.data[0].tradeNonceStr,
+              package: response.data[0].tradeId,
+              signType: 'MD5',
+              paySign: response.data[0].tradeOpenId,
+              // total_fee: response.data[0].total_fee,
+              success(res) {
+                console.log(res)
+              },
+              fail(res) {
+                console.log(res)
+              }
+          })
+        }
+      }
+    })
+    
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     let that=this;
-    let order_id=options.order_id;
+    let order_id=options.orderId;
+    this.obtain_order_detail(order_id)
     that.setData({
       order_id:order_id
     })
