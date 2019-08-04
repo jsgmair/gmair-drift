9c6e7dc7d1cc71174ad753ff864949f81ab6afc6
lawrenceZL <40664153+lawrenceZL@users.noreply.github.com>
2019-08-04 08:07:08 +0800

    扫二维码

--- pages/obtain_credit/obtain_credit.js
+++ pages/obtain_credit/obtain_credit.js
@@ -1,28 +1,33 @@
-// pages/order_list/order_list.js
-const app = getApp();
+// pages/obtain_credit/obtain_credit.js
+var QRCode = require('../../utils/weapp-qrcode.js');
+var qrcode;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+      web_src:'https://microservice.gmair.net/drift/page/credit',
+  },
 
+  backToActivity(e){
+    wx.navigateBack({
+      delta: 1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let openid = wx.getStorageSync('openid')
-    wx.request({
-      url: app.globalData.protocol + app.globalData.url + '/drift/order/'+openid+'/list',
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
-      },
-      success: function (response) {
-        console.log(response);
-      }
-    })
+    qrcode = new QRCode('canvas', {
+      text: this.data.web_src,
+      width: 150,
+      height: 150,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
   },
 
   /**
